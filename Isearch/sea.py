1e8baf83dcb773e9ed287a3a459b0c4a51207907
Izzokz <144491055+Izzokz@users.noreply.github.com>
2024-06-05 10:13:06 +0200

    updated but firefox seems to bug with Linux

--- Isearch/sea.py
+++ Isearch/sea.py
@@ -1,22 +1,37 @@
-import argparse
 import subprocess
+import time
+import os
 
 keys = [
-    "What is ",
-    "How to ",
-    "Purposes of ",
-    "How popular is ",
-    "Cost of "
+    "What+is+",
+    "How+to+",
+    "Purposes+of+",
+    "How+popular+is+",
+    "Cost+of+"
 ]
 
-def search(text):
-    search = "https://www.google.com/search?client=firefox-b-lm&q="
-    for key in keys:
-        subprocess.run(["firefox", search + key + text], capture_output = True, text = True, check = True)
+device = os.name
 
-parser = argparse.ArgumentParser()
-parser.add_argument('search', type = str)
-try:
-    search(parser.parse_args().search)
-except:
-    subprocess.run([f"python3 {__file__.split('/')[-1]} {'-'.join(input('What kind of information do you want?').split())}"], shell = True)
+def search(text):
+    search = "https://www.google.com/search?q="
+    
+    if device == 'posix':
+        if subprocess.run(['ps aux | grep -q [f]irefox && echo "on" || echo "off"'], capture_output = True, shell = True).stdout == b'off\n':
+            timer = True
+        for key in keys:
+            subprocess.run(["firefox " + search + key + text + " 2>/dev/null"], shell = True)
+            if timer:
+                time.sleep(1)
+                timer = False
+    
+    if device == 'nt':
+        if subprocess.run(["PowerShell", "Get-Process firefox -ErrorAction SilentlyContinue"], capture_output = True, shell = True).stdout == b'':
+            timer = True
+        for key in keys:
+            subprocess.run(["PowerShell", "start firefox " + search + key + text], shell = True)
+            if timer:
+                time.sleep(1)
+                timer = False
+
+query = "+".join(input("What kind of information do you want?").split())
+search(query)
